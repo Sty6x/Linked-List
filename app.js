ea61a8db547362046e660f7cbd728976c0f09ba5
diaz <jerylfranz460@gmail.com>
2022-11-09 22:31:41 +0400

    when prepending a linked list need to assign it again because if we were to refer the old head this would not work if we expect it to have a new head

--- app.js
+++ app.js
@@ -9,14 +9,27 @@ class LinkedList {
     tmp.nextNode = new Node(value)
   }
   prependNode(node, value = 'Prepended Node') {
-    let tmp = new Node(value);
-    tmp.nextNode = node;
-    console.log(tmp)
+    let tmp = new Node(value)
+    tmp.nextNode = node
+    return tmp
   }
-
+  size(node) {
+    let i = 0;
+    let tmp = node
+    while (tmp !== null) {
+      tmp = tmp.nextNode
+      i++
+    }
+    return `There currently are ${i} nodes in the list`
+  }
+  head(node) {
+    let tmp = node;
+    tmp.nextNode = null
+    return tmp
+  }
 }
 class Node {
-  constructor(data) {
+  constructor(data, nextNode) {
     this.data = data;
     this.nextNode = null;
   }
@@ -26,7 +39,8 @@ let head = new Node('Head Node')
 let node1 = new Node('Node 1');
 let node2 = new Node('Node 2');
 head.nextNode = node1;
+node1.nextNode = node2;
 LList.appendNode(head, 'Node 3')
 LList.appendNode(head)
-LList.prependNode(head)
+let newHead = LList.prependNode(head, 'New Head');
 console.log(head)
