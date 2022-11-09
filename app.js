b5eb354c124384a26146e69065480712b5e31c3a
diaz <jerylfranz460@gmail.com>
2022-11-09 21:50:01 +0400

    appending work, when traversing nodes make sure you check the next node instead of current node

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
 #!usr/bin/node
 
-class LinkedList{
-  appendNode(node){
+class LinkedList {
+  appendNode(node, value = 'Appended Node') {
     let tmp = node;
-    while(tmp !== null){
-      console.log(tmp)
+    while (tmp.nextNode !== null) {
       tmp = tmp.nextNode
-      }
+    }
+    tmp.nextNode = new Node(value)
+  }
+  prependNode(node, value = 'Prepended Node') {
+    let tmp = new Node(value);
+    tmp.nextNode = node;
     console.log(tmp)
   }
-  
+
 }
 class Node {
-  constructor(data){
+  constructor(data) {
     this.data = data;
     this.nextNode = null;
   }
 }
-const LList = new LinkedList() 
+const LList = new LinkedList()
 let head = new Node('Head Node')
 let node1 = new Node('Node 1');
 let node2 = new Node('Node 2');
 head.nextNode = node1;
-console.log(head)
+LList.appendNode(head, 'Node 3')
 LList.appendNode(head)
+LList.prependNode(head)
 console.log(head)
-
